c6154018fec9b670e5f722767c616f96ad1a247a
skbashir <105426479+skbashir@users.noreply.github.com>
2022-05-19 17:34:45 +0530

    Add files via upload

---
+++ practical/Constructor.java
@@ -0,0 +1,14 @@
+package zeesh;
+
+public class Constructor {
+	public Constructor() {
+System.out.println("This is default constructor");
+
+	}
+	
+	public static void main(String[] args) {
+Constructor mdc=new Constructor();
+
+	}
+	
+}
